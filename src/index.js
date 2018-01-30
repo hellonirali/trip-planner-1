b096ce1442f13717e4e8a40761dabed658a22008
Nirali Shah <hellonirali@Niralis-MacBook-Air.local>
2018-01-30 14:15:53 -0600

    finished part 1

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-console.log("Hello from JavaScript");
+const mapboxgl = require("mapbox-gl");
+const buildMarker = require("./marker");
+
+mapboxgl.accessToken = "pk.eyJ1IjoiaGVsbG9uaXJhbGkiLCJhIjoiY2pkMXRoemw5MzJmYTJ5bzV3aDl5eW5mOSJ9.DnLuJ3_CNU3oX3zje0vjHQ";
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: [-87.6354, 41.8885], //[-74.009, 40.705] FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  zoom: 12, // starting zoom
+  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+});
+
+// const markerDomEl = document.createElement("div"); // Create a new, detached DIV
+// markerDomEl.style.width = "32px";
+// markerDomEl.style.height = "39px";
+// markerDomEl.style.backgroundImage = "url(http://i.imgur.com/WbMOfMl.png)";
+
+// new mapboxgl.Marker(markerDomEl).setLngLat([-87.6354, 41.8885]).addTo(map); // [-87.6354, 41.8885] for Chicago
+
+const marker = buildMarker("activity", [-87.6354, 41.8885]); // or [-87.6354, 41.8885]
+console.log(marker);
+marker.addTo(map);
